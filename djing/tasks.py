4c5bbfbf981b8db7191a2444a6395a238e6cb649
Dmitry Novikov <nerosketch@gmail.com>
2019-10-23 00:54:43 +0300

    optymize multicast_email_notify

--- djing/tasks.py
+++ djing/tasks.py
@@ -36,23 +36,17 @@ def send_email_notify(msg_text: str, account_id: int):
 @shared_task
 def multicast_email_notify(msg_text: str, account_ids: Iterable):
     text_content = strip_tags(msg_text)
-    for acc_id in account_ids:
-        if not acc_id:
-            continue
-        try:
-            account = UserProfile.objects.get(pk=acc_id)
-            target_email = account.email
-            msg = EmailMultiAlternatives(
-                subject=getattr(settings, 'COMPANY_NAME', 'Djing notify'),
-                body=text_content,
-                from_email=getattr(settings, 'DEFAULT_FROM_EMAIL'),
-                to=(target_email,)
-            )
-            msg.attach_alternative(msg_text, 'text/html')
-            msg.send()
-        except SMTPException as e:
-            logging.error('SMTPException: %s' % e)
-        except gaierror as e:
-            logging.error('Socket error: %s' % e)
-        except UserProfile.DoesNotExist:
-            logging.error('UserProfile with pk=%d not found' % acc_id)
+    targets = [usr.email for usr in UserProfile.objects.filter(pk__in=account_ids).only('email').iterator()]
+    try:
+        msg = EmailMultiAlternatives(
+            subject=getattr(settings, 'COMPANY_NAME', 'Djing notify'),
+            body=text_content,
+            from_email=getattr(settings, 'DEFAULT_FROM_EMAIL'),
+            to=targets
+        )
+        msg.attach_alternative(msg_text, 'text/html')
+        msg.send()
+    except SMTPException as e:
+        logging.error('SMTPException: %s' % e)
+    except gaierror as e:
+        logging.error('Socket error: %s' % e)
